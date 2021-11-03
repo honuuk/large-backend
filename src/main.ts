dc3b48b64932208bcbcc640b381e86607faf0b9c
jewook <jewook3617@gmail.com>
2021-11-03 09:18:55 +0900

    add: swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
+import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+
+function setupSwagger(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('Large API')
+    .setDescription('This is the API used for the blog platform "Large".')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  const path = 'api';
+  SwaggerModule.setup(path, app, document);
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  setupSwagger(app);
+
   await app.listen(3000);
 }
 bootstrap();
